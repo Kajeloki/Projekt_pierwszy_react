8d863b5c1867655d83c2625fd46c8fc7c37784a4
Kajeloki <piotrkw200@wp.pl>
2023-06-23 16:18:23 +0200

    poprawiony komponent wydatku

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,11 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-
- 
+const date=props.date;
     return (
         <div className="expense-item">
-            <div > 
-            {props.date.toISOString()}
-            </div>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description' >
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount} zł</div>
