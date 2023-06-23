6a4add3389b157e0a0d6366241f7ac61b9f3bf04
Kajeloki <piotrkw200@wp.pl>
2023-06-23 14:52:00 +0200

    komponent pojedynczego wydatku - podstawy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
+
+  const expenses=[
+    {
+      id:'e1',
+title: 'Ubezpieczenie samochodu',
+price: 1000,
+date: new Date(2023, 5, 23)
+    },
+    {
+      id:'e2',
+title: 'Ubezpieczenie zdrowotne',
+price: 1000,
+date: new Date(2023, 5, 23)
+    }, 
+    {
+      id:'e3',
+title: 'Ubezpieczenie nnw',
+price: 1000,
+date: new Date(2023, 5, 23)
+    },
+    {
+      id:'e4',
+title: 'Ubezpieczenie firmy',
+price: 1000,
+date: new Date(2023, 5, 23)
+    },
+  ]
+  
   return (
     <div>
-      <h2>Let's get started!</h2>
+      
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].price} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].price} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].price} date={expenses[2].date}></ExpenseItem>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].price} date={expenses[3].date}></ExpenseItem>
     </div>
   );
 }
