583f7b93389e15e8dca3d070b988099fe2a836d1
Kajeloki <piotrkw200@wp.pl>
2023-06-27 22:08:29 +0200

    parent-children / children-parent

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,11 +2,19 @@ import Card from "../UI/Card";
 import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css';
 
-const NewExpense = () => {
-return  <div className='new-expense'>
-   <ExpenseForm></ExpenseForm>
+const NewExpense = (props) => {
+   const saveExpenseDataHandler = (enteredExpenseData) => {
+      const expenseData = {
+         ...enteredExpenseData,
+         id: Math.random().toString()
+      };
+      props.onAddExpense(expenseData);
+   };
+
+   return <div className='new-expense'>
+      <ExpenseForm onSaveExpensesData={saveExpenseDataHandler}></ExpenseForm>
 
 
-</div>
+   </div >
 }
 export default NewExpense;
