8404ddd06638130606ba951f28372233a3715535
Kajeloki <piotrkw200@wp.pl>
2023-06-28 17:05:25 +0200

    dynamiczna lista wydatków

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,20 +4,13 @@ import Card from '../UI/Card';
 import React, {useState} from 'react';
 
 const ExpenseItem = (props) => {
-    const [title, setTitle]=useState(props.title);
-    //let title=props.title;
-   const ClickHandler = () => {
-     setTitle("nowy tytuł");
-   };
-
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className='expense-item__description' >
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount} zł</div>
             </div>
-            <button onClick={ClickHandler}>Przycisk</button>
         </Card>
     );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,12 +15,10 @@ const Expenses = (props) => {
         <div>
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onFilterChangeHandler={ExpensesDatesHandler}>
-                <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].price} date={props.expenses[0].date}></ExpenseItem>
-                <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].price} date={props.expenses[1].date}></ExpenseItem>
-                <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].price} date={props.expenses[2].date}></ExpenseItem>
-                <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].price} date={props.expenses[3].date}></ExpenseItem>
-            </ExpensesFilter>
 
+
+            </ExpensesFilter>
+            {props.expenses.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />)}
         </Card>
         </div>
 
